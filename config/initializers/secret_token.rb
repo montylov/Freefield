9b808dc5ee403793f8f4d7a6372b2c24d47c387e
MontyLov <montylov@gmail.com>
2013-03-22 12:24:09 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfield::Application.config.secret_token = '861a31c1e6d642a29c4538a605bef4e6c066fd2fe261d8a1125a6cc8e91f3234a6c01ae7bed9105e89cf6b4093f364c9a5435e9f6209c8a410f1d4a8f83d0907'
